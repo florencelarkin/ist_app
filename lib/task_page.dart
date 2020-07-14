a1e46b9fb084c263c2d0bd781493b2ec2e388967
florencelarkin <larki124@umn.edu>
2020-07-14 17:24:53 -0500

    got rid of variables I wasn't using

--- lib/task_page.dart
+++ lib/task_page.dart
@@ -17,16 +17,10 @@ class _TaskPageState extends State<TaskPage> {
   Map<int, int> gridMap  = {0: 0, 1: 0, 2: 0, 3: 0, 4:0,5:0, 6: 0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0,
     13:0, 14:0, 15:0, 16:0, 17:0, 18:0, 19:0, 20:0, 21:0, 22:0, 23:0, 24:0};
 
-  int selectedIndex = 0;
   int yellowCount = 0;
   int blueCount = 0;
-  int pressCount = 0;
 
   List<bool> openedSquares;
-
-  onSelected (int index) {
-    setState(() => selectedIndex = index);
-  }
 
   void getSquareColor (int index) {
     int randomNumber = Random().nextInt(2) + 1;
