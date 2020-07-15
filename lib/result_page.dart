96582e08f8299e67cbe0a7e26049c6006d3c00ce
florencelarkin <larki124@umn.edu>
2020-07-15 00:50:55 -0500

    added bottom buttons and result page

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -3,9 +3,27 @@ import 'package:flutter/material.dart';
 class ResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-          'this is the result page so far'
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              alignment: Alignment.center,
+              child: Text(
+                'You Win!',
+              ),
+            ),
+          ),
+          FlatButton(
+            child: Text('RESTART'),
+            onPressed: (){
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
     );
   }
--- lib/task_page.dart
+++ lib/task_page.dart
@@ -14,15 +14,17 @@ class _TaskPageState extends State<TaskPage> {
   Color squareColor;
 
   Map<int, int> gridMap  = {0: 0, 1: 0, 2: 0, 3: 0, 4:0,5:0, 6: 0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0,
-    13:0, 14:0, 15:0, 16:0, 17:0, 18:0, 19:0, 20:0, 21:0, 22:0, 23:0, 24:0};
+    13:0, 14:0, 15:0, 16:0, 17:0, 18:0, 19:0, 20:0, 21:0, 22:0, 23:0, 24:0, 25:3, 26:3, 27:3,28:3, 29:3, 30:3, 31:1, 32:3, 33:2, 34:3};
 
   int yellowCount = 0;
   int blueCount = 0;
+  bool blueMajority;
+  bool yellowMajority;
 
   List<bool> openedSquares;
 
   void getSquareColor () {
-    for (var i = 0; i < gridMap.length; i++) {
+    for (var i = 0; i < 25; i++) {
       int randomNumber = Random().nextInt(2) + 1;
       if (randomNumber == 1) {
         gridMap.update(i, (int randomNumber) => 1);
@@ -32,6 +34,15 @@ class _TaskPageState extends State<TaskPage> {
         gridMap.update(i, (int randomNumber) => 2);
         blueCount++;
       }
+    }
+  }
+
+  void getCorrectMajority(int yellowCount, int blueCount, String majorityChoice) {
+    if (yellowCount > blueCount) {
+      blueMajority = false;
+    }
+    else {
+      blueMajority = true;
     }
   }
 
@@ -57,27 +68,47 @@ class _TaskPageState extends State<TaskPage> {
         title: Text(''),
       ),
       body: GridView.builder(
+          physics: NeverScrollableScrollPhysics(),
         itemCount: gridMap.length,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 5),
         itemBuilder: (context, index) {
-          if(openedSquares[index] == false){
-            squareColor = Colors.grey;
-          }
-          else{
-            if (gridMap[index] == 1) {
-              squareColor = Colors.yellow;
+            if (index < 25) {
+              if (openedSquares[index] == false) {
+                squareColor = Colors.grey;
+              }
+              else {
+                if (gridMap[index] == 1) {
+                  squareColor = Colors.yellow;
+                }
+                else if (gridMap[index] == 2) {
+                  squareColor = Colors.blue;
+                }
+              }
+            }
+            else if (gridMap[index] == 3){
+              squareColor = Color(0xFF0A0E21);
+            }
+            else if (gridMap[index] == 1) {
+                squareColor = Colors.yellow;
             }
             else if (gridMap[index] == 2) {
               squareColor = Colors.blue;
             }
-          }
+
           return GridSquare(
             color: squareColor,
             onPress: () {
               setState(() {
-                if(openedSquares[index] == false){
+                if(openedSquares[index] == false && index < 25){
                   openedSquares[index] = true;}
+                else if (index == 31) {
+
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => ResultPage(),),);
+                }
+                else if (index == 33) {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => ResultPage(),),);
+                }
                 else {}
               },);
             },
